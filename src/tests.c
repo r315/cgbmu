b4bfe14f4a916613d8a4194792587becf2a85e7d
r315 <38652@alunos.isel.ipl.pt>
2018-03-18 20:01:35 +0000

    ADD: Tests for scroll

--- src/tests.c
+++ src/tests.c
@@ -1,6 +1,7 @@
 
 
 #include <common.h>
+#include <string.h>
 #include "graphics.h"
 #include "dmgcpu.h"
 #include "video.h"
@@ -14,30 +15,31 @@
 
 char filename[100];
 void decode(void);
+extern const char bg_map[32 * 32];
+extern const char tilesData[0x1000];
 
 void testRun(void) {
-	while (readJoyPad() != 255) {		
+	while (readJoyPad() != 255) {
 		interrupts();
 		decode();
-		timer();			
+		timer();
 		video();
 	}
 }
 
-void testAll(void){    
-    testRom(ALL_TESTS_ROM);
+void testAll(void) {
+	testRom(ALL_TESTS_ROM);
 }
 
-void testRom(char *fn){
-    strcpy(filename, ROMS_BINARY_PATH);
-    strcat(filename, fn);
-    loadRom(filename);
-    testRun();
+void testRom(char *fn) {
+	strcpy(filename, ROMS_BINARY_PATH);
+	strcat(filename, fn);
+	loadRom(filename);
+	testRun();
 }
 
-
-void testButtons(void){
-	char *b,t;
+void testButtons(void) {
+	char *b, t;
 	printf("Buttons Test\n");
 	while (readJoyPad() != 255) {
 		b = "";
@@ -60,127 +62,103 @@ void testButtons(void){
 		case 0x0b:   b = "[  UP  ]  "; break;
 		case 0x07:   b = "[ DOWN ]  "; break;
 		}
-		if(*b !='\0')
-			printf("%s\n",b);
+		if (*b != '\0')
+			printf("%s\n", b);
 		SDL_Delay(20);
 	}
 }
 
 const char f_letter[] = {
-    0x78,0x78,
-    0x40,0x40,
-    0x40,0x40,
-    0x78,0x78,
-    0x40,0x40,
-    0x40,0x40,
-    0x40,0x40,
-    0x00,0x00
-};
-const char sprites[] = {
-    16,8,0,0,
-    16,16,0,SPRITE_FLAG_XFLIP,
-    16+8,8,0,SPRITE_FLAG_YFLIP,
-    16+8,16,0,(SPRITE_FLAG_XFLIP|SPRITE_FLAG_YFLIP)
-};
-extern uint8_t scanlinedata[160];
-void dumpLine(uint8_t *buf, uint8_t size){
-uint8_t i;
-    putchar('[');
-    for(i=0;i<size;i++, buf++){
-        if(*buf)
-		    printf("%02X",*buf);
-        else
-            printf("  ");        
-    }
-    printf("]\n");    
-}
-
-void testSpriteDataLine(void){
-char i;
-    memcpy(vram,f_letter,sizeof(f_letter));
-    memcpy(oam,sprites,sizeof(sprites));
-    IOOBP0 = IOOBP1 = 0xE4;
-
-for(i=0; i<16; i++){
-    IOLY = i;
-    scanOAM();
-    dumpLine(scanlinedata, 16);
-
-}
-
-
-}
-
-const char letters[] = {
-	0x38,0x38,		// A
-	0x44,0x44,
-	0x44,0x44,
-	0x7c,0x7c,
-	0x44,0x44,
-	0x44,0x44,
-	0x44,0x44,
-	0x00,0x00,
-
-
-	0x78,0x78,			// F
+	0x78,0x78,
 	0x40,0x40,
 	0x40,0x40,
 	0x78,0x78,
 	0x40,0x40,
 	0x40,0x40,
 	0x40,0x40,
-	0x00,0x00,
-
-	0xfe,0xfe,
-	0x02,0xc2,
-	0x82,0x82,
-	0x82,0x82,
-	0x82,0x82,
-	0x82,0x82,
-	0xfe,0xfe,
-	0x00,0x00,
-
-	0xff,0xff,
-	0xff,0xff,
-	0xff,0xff,
-	0xff,0xff,
-	0xff,0xff,
-	0xff,0xff,
-	0xff,0xff,
-	0xff,0xff,
+	0x00,0x00
 };
-#define _P2 2
-#define _P0 3
-#define _P1 1
-#define _P3 3
-
-const char bg_map[] = {
-	_P0,_P0,_P0,_P0,_P0,_P0,_P0,_P0,_P1,_P1,_P1,_P1,_P1,_P1,_P1,_P1,_P2,_P2,_P2,_P2,_P2,_P2,_P2,_P2,_P3,_P3,_P3,_P3,_P3,_P3,_P3,_P3,	
-	_P1,_P1,_P1,_P1,_P1,_P1,_P1,_P1,_P2,_P2,_P2,_P2,_P2,_P2,_P2,_P2,_P3,_P3,_P3,_P3,_P3,_P3,_P3,_P3,_P0,_P0,_P0,_P0,_P0,_P0,_P0,_P0
+const char sprites[] = {
+	16,8,0,0,
+	16,16,0,SPRITE_FLAG_XFLIP,
+	16 + 8,8,0,SPRITE_FLAG_YFLIP,
+	16 + 8,16,0,(SPRITE_FLAG_XFLIP | SPRITE_FLAG_YFLIP)
 };
+extern uint8_t scanlinedata[160];
+void dumpLine(uint8_t *buf, uint8_t size) {
+	uint8_t i;
+	putchar('[');
+	for (i = 0; i < size; i++, buf++) {
+		if (*buf)
+			printf("%02X", *buf);
+		else
+			printf("  ");
+	}
+	printf("]\n");
+}
+
+void testSpriteDataLine(void) {
+	char i;
+	memcpy(vram, f_letter, sizeof(f_letter));
+	memcpy(oam, sprites, sizeof(sprites));
+	IOOBP0 = IOOBP1 = 0xE4;
+
+	for (i = 0; i < 16; i++) {
+		IOLY = i;
+		scanOAM();
+		dumpLine(scanlinedata, 16);
+
+	}
+}
 
 void testBgDataLine(void) {
 	char i;
-	memcpy(vram, letters, sizeof(letters));
+	memcpy(vram, tilesData, sizeof(tilesData));
 	memcpy((vram + TILE_MAP0_BASE), bg_map, sizeof(bg_map));
 	IOBGP = IOOBP0 = IOOBP1 = 0xE4;
 	IOLCDC |= BG_W_DATA;
 
-	for (i = 0; i<1; i++) {
+	for (i = 0; i < 1; i++) {
 		IOLY = i;
 		scanline();
 		dumpLine(scanlinedata, 32);
 
 	}
+}
 
+void Test_DBG_BGmap(void) {
+	memcpy(vram, tilesData, sizeof(tilesData));
+	memcpy((vram + TILE_MAP0_BASE), bg_map, sizeof(bg_map));
+	IOBGP = IOOBP0 = IOOBP1 = 0xE4;
+	IOLCDC |= BG_W_DATA;
+	IOSCX = 0;
+	IOSCY = 1;
 
+	while (1) {
+		//DBG_BGmap();
+		LCD_Window(0, 0, SCREEN_W, SCREEN_H);
+		for (IOLY = 0; IOLY < SCREEN_H; IOLY++) {
+			memset(scanlinedata, 0, sizeof(scanlinedata));
+			scanline();
+		}
+
+		switch (readJoyPad()) {
+		case 255: return;
+		case J_UP: IOSCY--; printf("SCY: %X\n", IOSCY); break;
+		case J_DOWN: IOSCY++; printf("SCY: %X\n", IOSCY); break;
+		case J_LEFT: IOSCX--; printf("SCX: %X\n", IOSCX); break;
+		case J_RIGHT: IOSCX++; printf("SCX: %X\n", IOSCX); break;
+		}
+		DelayMs(50);
+	}
 }
 
 
-void testMain (void){
-    //testSpriteDataLine();
+
+void testMain(void) {
+	//testSpriteDataLine();
 	//testBgDataLine();
-	testButtons();
+	//testButtons();
+	Test_DBG_BGmap();
 	exit(0);
 }
-    
