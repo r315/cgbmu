60643dab65e955cc555708100ce44ac70b2a0fcc
r315 <38652@alunos.isel.ipl.pt>
2018-03-20 01:13:20 +0000

    CHG: scanOAM now saves objects to array and these are placed at scanllinedata after background and window,
    implement obj priority for background

--- dmgcpu/video.c
+++ dmgcpu/video.c
@@ -7,43 +7,53 @@
 const unsigned short lcd_pal[] = { 0xE7DA,0x8E0E,0x334A,0x08C4 };
 
 uint16_t video_cycles = 0;
-Sprite *spriteline[MAX_SPRITES / sizeof(Sprite)];
+Object *spriteline[MAX_OBJECTS / sizeof(Object)];
 uint8_t bgdataline[40];
 uint8_t scanlinedata[160];		// one line of pixels
+Object *objs[MAX_LINE_OBJECTS];
 
 //-----------------------------------------
 //put one line of Sprite data into scanlinedata
 //-----------------------------------------
-void putSpriteData(Sprite *sp, uint8_t *dst) {
-	uint8_t i = SPRITE_W, color;
-	uint8_t pal = (sp->flags & SPRITE_FLAG_PAL) ? IOOBP1 : IOOBP0;
+void putObjectData(Object *obj, uint8_t *dst) {
+	uint8_t i = SPRITE_W, color, line;
+	uint8_t pal = (obj->flags & OBJECT_FLAG_PAL) ? IOOBP1 : IOOBP0;
 	//Each Tile has 16bytes and one line os pixels neads 2 bytes
-	uint8_t *p = (uint8_t*)(vram + (sp->pattern * sizeof(TileData)));	//get tiledata base
-	// add line offset, 2byte per line
-	p += ((sp->flags & SPRITE_FLAG_YFLIP) ? (7 - (IOLY & TILE_LINE_MASK)) : (IOLY & TILE_LINE_MASK)) << 1;
-	uint8_t lsb = *p++;
-	uint8_t msb = *p;
+	TileData *td = (TileData*)vram + obj->pattern;
+	// Add line offset, 2byte per line
+	line = (obj->flags & OBJECT_FLAG_YFLIP) ? (7 - TILE_LINE(IOLY)) : TILE_LINE(IOLY);
 
-	if (sp->flags & SPRITE_FLAG_XFLIP) {
+	uint8_t lsb = td->line[line].lsb;
+	uint8_t msb = td->line[line].msb;
+
+	dst += obj->x - SPRITE_W;
+
+	if (obj->flags & OBJECT_FLAG_XFLIP) {		
 		while (i--) {
-			color = (msb >> (7 - i)) & 1;
-			color <<= 1;
-			color |= (lsb >> (7 - i)) & 1;
-			color = (pal >> (color << 1)) & 3;  // pal uses 2 bit per color
-			if (color)
-				*dst = color;
-			dst += 1;
+			color = (msb & 0x1) ? 2 : 0;
+			color |= (lsb & 0x1) ? 1 : 0;
+			msb >>= 1;
+			lsb >>= 1;
+			color = (pal >> (color << 1)) & 3;
+			if (color) {
+				if(!(obj->flags & OBJECT_FLAG_PRIO) || !*dst)
+					*dst = color;
+			}
+			dst++;
 		}
 	}
 	else {
 		while (i--) {
-			color = (msb >> i) & 1;
-			color <<= 1;
-			color |= (lsb >> i) & 1;
+			color = (msb & 0x80) ? 2 : 0;
+			color |= (lsb & 0x80) ? 1 : 0;
+			msb <<= 1;
+			lsb <<= 1;
 			color = (pal >> (color << 1)) & 3;
-			if (color)
-				*dst = color;
-			dst += 1;
+			if (color) {
+				if (!(obj->flags & OBJECT_FLAG_PRIO) || !*dst)
+					*dst = color;
+			}
+			dst++;
 		}
 	}
 }
@@ -56,7 +66,7 @@ void putSpriteData(Sprite *sp, uint8_t *dst) {
 * @param line           the line to put
 * @param size			SCREEN_W for background, WX for window (not implemented yet)
 *---------------------------------------------------------*/
-void putTileData(uint8_t *tilemapline, uint8_t *dst, uint8_t pixeloffset, uint8_t line, uint8_t size, uint8_t transparent) {
+void putTileData(uint8_t *tilemapline, uint8_t *dst, uint8_t pixeloffset, uint8_t line, uint8_t size) {
 	uint8_t tileindex, msb, lsb, color;
 	TileData *td;
 
@@ -70,21 +80,13 @@ void putTileData(uint8_t *tilemapline, uint8_t *dst, uint8_t pixeloffset, uint8_
 		msb <<= TILE_LINE(pixeloffset);
 		lsb <<= TILE_LINE(pixeloffset);
 
-		do {
-			if (transparent == ON) {
-				if (!*dst)
-					goto putpixel;
-			}
-			else {
-			putpixel:
-				color = (msb & 0x80) ? 2 : 0;
-				color |= (lsb & 0x80) ? 1 : 0;
-				msb <<= 1;
-				lsb <<= 1;
-				color = (IOBGP >> (color << 1)) & 3;
-				*dst = color;
-			}				
-			dst += 1;
+		do {			
+			color = (msb & 0x80) ? 2 : 0;
+			color |= (lsb & 0x80) ? 1 : 0;
+			msb <<= 1;
+			lsb <<= 1;
+			color = (IOBGP >> (color << 1)) & 3;
+			*(dst++) = color;	
 			pixeloffset++;
 			size--;
 		} while (TILE_PIXEL(pixeloffset) != 0 && size != 0);
@@ -95,23 +97,29 @@ void putTileData(uint8_t *tilemapline, uint8_t *dst, uint8_t pixeloffset, uint8_
 // read OBJECT Attribute Memory for one line
 //-----------------------------------------
 void scanOAM() {
-	uint8_t i, n, tileline = (IOLY + 16) >> 3;	// Y position has a offset of 16pixels
-	Sprite *poam = (Sprite*)&oam[0];
-
-	memset(scanlinedata, 0, sizeof(scanlinedata));
+	uint8_t i, n, objline = IOLY + 16;	// Y position has a offset of 16pixels
+	Object *pobj = (Object*)oam;
 
 	n = 0;
-	for (i = 0; i < MAX_SPRITES; i++) {
-		if (tileline == (poam->y >> 3)) {
-			if (poam->x >= SPRITE_W && poam->x < SCREEN_W + SPRITE_W) {
-				putSpriteData(poam, scanlinedata + poam->x - 8);
-				n++;
+	for (i = 0; i < MAX_OBJECTS; i++, pobj++) {
+		if (pobj->x >= SPRITE_W && pobj->x < SCREEN_W + SPRITE_W) {
+			if (IOLCDC & OBJ_SIZE) {	// 8x16 objs
+				if (objline >= pobj->y && objline < (pobj->y + (SPRITE_H * 2))) {
+					objs[n] = pobj;
+					n++;
+				}
 			}
-		}
-		poam += 1;
-		if (n >= MAX_LINE_SPRITES)
+			else {
+				if (objline >= pobj->y && objline < (pobj->y + SPRITE_W)) {
+					objs[n] = pobj;
+					n++;
+				}
+			}
+		}		
+		if (n >= MAX_LINE_OBJECTS)
 			break;
 	}
+	objs[n] = NULL;
 }
 //-----------------------------------------
 //
@@ -124,11 +132,11 @@ void scanline() {
 	// Get tile map base
 	tilemapline = (uint8_t*)(vram + ((IOLCDC & BG_MAP) ? TILE_MAP1_BASE : TILE_MAP0_BASE));
 	// Add line and scroll-y offset for getting tile pattern	
-	//bgmapline += (TILE_LINE_INDEX(IOLY) + TILE_LINE_INDEX(IOSCY)) & BG_SIZE_MASK;
 	line = (uint8_t)(IOLY + IOSCY);
 	tilemapline += TILE_LINE_INDEX(line);
 
-	putTileData(tilemapline, sld, IOSCX, line, SCREEN_W, ON);
+	memset(scanlinedata, 0, sizeof(scanlinedata));
+	putTileData(tilemapline, sld, IOSCX, line, SCREEN_W);	
 
 	if (IOLCDC & W_DISPLAY && IOLY >= IOWY && IOWX < SCREEN_W + 7) 
 	{
@@ -136,8 +144,12 @@ void scanline() {
 		sld = scanlinedata + IOWX - 7;				//destination offset given by IOWX, WX has an offset of 7
 		tilemapline = (uint8_t*)(vram + ((IOLCDC & W_MAP) ? TILE_MAP1_BASE : TILE_MAP0_BASE));
 		tilemapline += TILE_LINE_INDEX(line);
-		putTileData(tilemapline, sld, 0, line, SCREEN_W - IOWX + 7, OFF);
+		putTileData(tilemapline, sld, 0, line, SCREEN_W - IOWX + 7);
 	}
+
+	pixel = 0;
+	while (objs[pixel] != NULL)
+		putObjectData(objs[pixel++], scanlinedata);
 
 	sld = scanlinedata;
 	for (pixel = 0; pixel < SCREEN_W; pixel++, sld++) {
